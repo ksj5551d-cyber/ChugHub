f4601bb2984cf37cf5f3a5a7b344d405445ecd54
ksj5551d-cyber <ksj5551d@gmail.com>
2025-11-11 12:20:50 +0530

    all  files

---
+++ .history/js practice/practice_20251106105726.js
@@ -0,0 +1,24 @@
+
+// armstrong number//
+
+
+
+function pal (arm){
+  let str = arm .toString();
+  let lth =str.length;
+  let res=0;
+  for (i=0;i<lth;i++){
+    res+=(str[i])**lth;
+    
+
+  } if (arm===res) {
+    console.log("number is armstong");return res
+  }else{
+    return -1
+  }
+}
+
+let userInput = prompt("enter a number:");
+console.log(pal(Number(userInput)));
+
+
